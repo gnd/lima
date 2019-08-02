29c46a0b8516d4685790384421fee88d9a22a9bc
gnd <gnd@itchybit.org>
2019-08-02 13:11:47 +0200

    adding installer script

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ else
 fi
 
 # Install prerequisities
-apt-get install python git libvirt-clients libvirt-daemon libvirt-daemon-system net-tools
+apt-get install python git libvirt-clients libvirt-daemon libvirt-daemon-system net-tools qemu-kvm
 
 # Create directory structure
 echo "Creating directory structure:"
