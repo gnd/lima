f162df59354a8aa1ca8df6061db0b75867d2f3e1
gnd <gnd@itchybit.org>
2019-08-05 22:56:25 +0200

    updating create-vm

--- ipgen.py
+++ ipgen.py
@@ -5,8 +5,8 @@ import random
 import os.path
 
 # parse the machine type
-vmtype = sys.argv[0]
-vmlist = sys.argv[1]
+vmtype = sys.argv[1]
+vmlist = sys.argv[2]
 
 # generate IP according to machine type
 def genIP(vmtype, vmlist):
@@ -21,7 +21,7 @@ def genIP(vmtype, vmlist):
 		for vm in vms:
 			attr = vm.split()
 			if (((attr[4] == 'sta') and (vmtype == 'static')) or ((attr[4] == 'dyn') and (vmtype == 'dynamic'))):
-				used.append(attr[0].split['-'][2])
+				used.append(attr[0].split('-')[2])
 
 		# find the first free number
 		for i in range(100,199):
