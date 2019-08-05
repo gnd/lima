38c1204dc74b2cef4c14021c3ee6e767a3ed004c
gnd <gnd@itchybit.org>
2019-08-05 14:52:12 +0200

    updating create-vm

--- create-vm.sh
+++ create-vm.sh
@@ -62,9 +62,8 @@ connect-ssh() {
 		if [[ ! "$check" == "$hostname" ]]; then
 			clean_line
             tries=$((tries+1))
-			print_tries=$((tries*10))
             for (( try=1; try<=tries; try=try+1 )); do
-                printf "Waiting for VM: "$print_tries"s "; draw_tries
+                printf "Waiting for VM: "$tries"s "; draw_tries
                 clean_line
             done
 		else
