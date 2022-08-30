7a44db308ee35d2fc6fb142c5849ce84a6326bd5
gnd <gnd@itchybit.org>
2022-08-30 22:38:27 +0200

    Bad variable name in extend-disk

--- extend-disk.sh
+++ extend-disk.sh
@@ -101,7 +101,7 @@ echo '</disk>' >> $TMPFILE
 
 echo "Modifying the VM definition file"
 # hardcoded for now ;/
-sed -i '0,/<disk type="file" device="disk">/s/<disk type="file" device="disk">/<disk type="file" device="disk">\n      <driver name="qemu" type="qcow2"\/>\n      <source file="\/data\/pool\/vms\/'$VM_TYPE'\/'$NAME'\/disk-'$NXT'.img"\/>\n      <target dev="vd'$NXT'" bus="virtio"\/>\n    <\/disk>\n    <disk type="file" device="disk">/g' $VM_XML
+sed -i '0,/<disk type="file" device="disk">/s/<disk type="file" device="disk">/<disk type="file" device="disk">\n      <driver name="qemu" type="qcow2"\/>\n      <source file="\/data\/pool\/vms\/'$VM_TYPE'\/'$VM_NAME'\/disk-'$NXT'.img"\/>\n      <target dev="vd'$NXT'" bus="virtio"\/>\n    <\/disk>\n    <disk type="file" device="disk">/g' $VM_XML
 
 # Adding the new disk to the machine
 virsh attach-device $VM_NAME $TMPFILE
